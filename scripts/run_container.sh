f744484b9d72238f2c32a22644e27a027cff81ea
Christopher Antila <christopher@antila.ca>
2018-01-24 21:11:23 -0500

    Add 'sudo' to installed packages

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -3,6 +3,8 @@
 # Doing this in a separate script lets us do it step by step while using a
 # single docker layer.
 
+dnf update -y
 dnf install -y flatpak flatpak-builder
 dnf install -y git
+dnf install -y sudo
 dnf clean all
